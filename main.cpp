d725402311bc623416b2deb5e8884d6f3ceb890d
Alex_laptop <aleksandriliev05@gmail.com>
2024-01-27 23:06:00 +0200

    Made sure the code i added works.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
 #include ".include/game_layer.h"
 #include ".include/broad_sword.h"
+#include ".include/visualizer.h"
+
+// writes a cell value and catches an error
+void game_layer_test_a()
+{
+	GameLayer jim;
+	std::cout<<"Get cell, set id to 10, save it to the board:"<<std::endl;
+	auto cell = jim.getCell(0,0);
+	std::cout<<"Cell id: "<<cell.id<<std::endl;
+	std::cout<<"Modifying cell..."<<std::endl;
+	cell.id = 1;
+	jim.setCell(0,0,cell);
+	std::cout<<"Cell new id: "<<jim.getCell(0,0).id<<std::endl;
+	std::cout<<"Attempting access at x7,y11..."<<std::endl;
+	try{
+		jim.getCell(7,11);
+	}catch(const std::exception& e){
+		std::cout<<e.what()<<std::endl;
+	};
+}
 
 void print_layer(GameLayer layer)
 {
@@ -23,25 +43,13 @@ void print_layer(GameLayer layer)
 	}
 }
 
-int main(int argc, char **argv)
+// uses a weapon
+void game_layer_test_b()
 {
-	GameLayer jim;
-	std::cout<<"Get cell, set id to 10, save it to the board:"<<std::endl;
-	auto cell = jim.getCell(0,0);
-	std::cout<<"Cell id: "<<cell.id<<std::endl;
-	std::cout<<"Modifying cell..."<<std::endl;
-	cell.id = 1;
-	jim.setCell(0,0,cell);
-	std::cout<<"Cell new id: "<<jim.getCell(0,0).id<<std::endl;
-	std::cout<<"Attempting access at x7,y11..."<<std::endl;
-	try{
-		jim.getCell(7,11);
-	}catch(const std::exception& e){
-		std::cout<<e.what()<<std::endl;
-	};
 	std::cout<<"Setting up game layer, giving cell 5,5 a broadsword, swinging right, then seeing what happens:"<<std::endl;
 	GameLayer layer;
 	print_layer(layer);
+	auto cell = layer.getBlankCell();
 	cell.stats.hp = 0;
 	layer.setCell(4,4, cell);
 	layer.setCell(4,5, cell);
@@ -57,6 +65,28 @@ int main(int argc, char **argv)
 	std::cout<<"4,4: "<<layer.getCell(4,4).stats.hp<<std::endl;
 	std::cout<<"4,5: "<<layer.getCell(4,5).stats.hp<<std::endl;
 	std::cout<<"4,6: "<<layer.getCell(4,6).stats.hp<<std::endl;
-	
+}
+
+// draws on the screen
+void graphic_module_test()
+{
+	SDL_Init(SDL_INIT_EVERYTHING);
+	Visualizer v;
+	Visual::Rect rect = {100,100,100,100};
+	VisualShape shape = VisualShape(rect);
+	shape.set_fullness(true);
+	shape.color = (Visual::Color){RGBA_CYAN};
+	v.set_draw_color(RGBA_WHITE);
+	v.flush();
+	v.draw_shape(shape);
+	v.present();
+	int stall;
+	std::cin>>stall;
+	SDL_Quit();
+}
+
+int main(int argc, char **argv)
+{
+	graphic_module_test();
 	return 0;
 }
